6e419fe8066dfe6b9f446fb960f8a285f62155b2
Kyaara <7339.goyal@gmail.com>
2020-10-11 11:48:16 -0500

    resetandrank

--- js/Game.js
+++ js/Game.js
@@ -45,7 +45,7 @@ class Game {
     form.hide();
     
     Player.getPlayerInfo();
-    
+    player.getcarsatend();
     if(allPlayers !== undefined){
       background("brown");
       
@@ -88,6 +88,8 @@ class Game {
 
     if(player.distance>3650){
       gameState=2;
+      player.rank=player.rank+1;
+      Player.updatecarsatend(player.rank);
     }
 
     drawSprites();
@@ -96,6 +98,7 @@ class Game {
   
     end(){
       console.log("game ended");
+      console.log(player.rank);
     }
 
   
